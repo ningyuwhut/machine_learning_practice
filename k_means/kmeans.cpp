fdcb5a318e1eede60d9a52c8273b84741fc63e82
ningyuwhut <ningyuwhut@gmail.com>
2015-12-19 11:57:36 +0800

    add kmeans

--- k_means/kmeans.cpp
+++ k_means/kmeans.cpp
@@ -4,9 +4,11 @@
 #include <algorithm>
 #include <float.h>
 #include <math.h>
+#include <map>
 using namespace std;
 
-void kmeans(int** dataset, int sample_number, int feature_number, int cluster_number);
+//应该返回聚类中心和每个样本所属的中心
+int** kmeans(int** dataset, int sample_number, int feature_number, int cluster_number);
 int main(int argc, char** argv){
 
 }
@@ -36,9 +38,17 @@ int** kmeans(int** dataset, int sample_number, int feature_number, int cluster_n
 	}
     }
 
-    vector<int> cluster_of_samples;
+    vector<int> cluster_of_samples;//记录每个样本所属的聚类中心
+    int iter=0;
+    int max_iter=500;
+    //三个终止条件
+    //1.达到迭代次数上限
+    //2.类内元素不再变化或者低于最低比例
+    //3.每个样本到各自的聚类中心的欧氏距离之和(RSS)低于某个阈值
+    float rss=0.0;
     while( true ){
 	//每个样本计算和每个聚类中心的距离，从而找到最近的那个聚类中心
+	float new_rss=0.0;
 	for( int i=0; i< sample_number; ++i ){
 	    float minimum_distance=FLT_MAX;
 	    int corr_cluster=-1;
@@ -50,12 +60,51 @@ int** kmeans(int** dataset, int sample_number, int feature_number, int cluster_n
 		if( distance < minimum_distance ){
 		    minimum_distance=distance;
 		    corr_cluster=j;
+		    new_rss+=minimum_distance;
 		}
 	    }
 	    cluster_of_samples.push_back(corr_cluster);
 	}
-	for( vector<int>::iterator iter=
+	//rss变化比例低于0.001就退出
+	if( abs(new_rss - rss ) / rss < 0.00 1 ){
+	    break;
+	}
+	//记录每个聚类中心下的样本
+	map<int, vector<int> > samples_in_each_cluster;
+	for( int i=0; i < cluster_of_samples.size(); ++i ){
+	    if( samples_in_each_cluster.find(cluster_of_samples[i]) != samples_in_each_cluster.end() ){
+		samples_in_each_cluster[cluster_of_samples[i] ].push_back(i);
+	    }else{
+		vector<int> sample_index;
+		sample_index.push_back(i);
+		samples_in_each_cluster[cluster_of_samples[i]]=sample_index;
+	    }
+	}
+	//计算每个聚类中心下的新中心
 
+	for( map<int, vector<int> > iter=samples_in_each_cluster.begin(); iter != samples_in_each_cluster.end(); ++iter){
+	    int cluster_index=iter->first;
+	    vector<int> sample_index=iter->second; //该类下的所有样本
 
+	    for(int i=0; i< cluster_number; ++i ){
+		for( int j =0; j< feature_number; ++j){
+		    clusters[i][j]=0.0;
+		}
+	    }
+	    for( int i =0; i < sample_index.size(); ++i ){
+		for( int j=0; j < feature_number; ++j ){
+		    clusters[cluster_index][j]+=dataset[sample_index[i]][j]
+		}
+	    }
+	    for( int i=0; i <=feature_number; ++j ){
+		clusters[cluster_index]/=sample_index.size();
+	    }
+	}
+
+	iter++;
+
+	if( iter > max_iter )
+	    break;
     }
+    return clusters;
 }
